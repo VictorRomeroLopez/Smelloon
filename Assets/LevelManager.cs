4849f0e249971bf8604a6353adfe3d6df830f00c
Andreu Margarit <andreu.margarit@enti.cat>
2020-03-21 19:06:46 +0100

    Player + Shelfs + Level Manager

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+
+    [SerializeField] private int minIngredientsAsked = 0;
+    [SerializeField] private float maxTime = 0;
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private List<GameObject> stars;  
+    private int currentIngredientsPicked = 0;
+
+    private bool ingredientsAskedCompleted = false;
+    private bool specialPicked = false;
+    private bool scaped = false;
+    private bool levelEnded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale= 1;
+        StartCoroutine(LevelTimer());
+        SpecialIngredientPicked();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IngredientPicked()
+    {
+        currentIngredientsPicked++;
+        if(minIngredientsAsked == currentIngredientsPicked)
+        {
+            ingredientsAskedCompleted = true;
+        }
+    }
+
+    public void SpecialIngredientPicked()
+    {
+        specialPicked = true;
+    }
+
+    public void ScapedAtTime()
+    {
+        scaped = true;
+    }
+
+    public void LevelEnded()
+    {
+        Time.timeScale = 0;
+        canvas.SetActive(true);
+        if (scaped) stars[0].SetActive(true);
+        if (ingredientsAskedCompleted) stars[1].SetActive(true);
+        if (specialPicked) stars[2].SetActive(true);
+    }
+
+    IEnumerator LevelTimer()
+    {
+        yield return new WaitForSeconds(maxTime);
+        if(!levelEnded)
+            LevelEnded();
+    }
+    
+}
---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    Rigidbody2D rB;
+    private float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += transform.up * Time.fixedDeltaTime*speed;
+            rB.velocity = Vector2.up * speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rB.velocity = -Vector2.up * speed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rB.velocity = -Vector2.right * speed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rB.velocity = Vector2.right * speed;
+        }
+        else
+        {
+            rB.velocity = Vector2.zero;
+        }
+
+        Debug.Log(rB.velocity);
+    }
+
+}
