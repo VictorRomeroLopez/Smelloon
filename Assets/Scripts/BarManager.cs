de55779c89cf2d7a5e5d7df75a661e4400988d68
VictorRomeroLopez <victor.romero@innovamat.es>
2020-03-21 17:45:15 +0100

    Initial commit

---
+++ Assets/Scripts/BarManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarManager : MonoBehaviour
+{
+    private Image _barImage;
+
+    public float Size { get; set; }
+    public int MinValue { get; set; }
+    public int MaxValue { get; set; }
+
+    public void UpdateBar()
+    {
+        float valueBar = MinValue;
+
+        if (MaxValue * Size > MinValue)
+        {
+            valueBar = MaxValue * Size;
+        }
+
+        _barImage.rectTransform.sizeDelta = new Vector2(25, valueBar);
+    }
+
+    private void OnValidate()
+    {
+        _barImage = GetComponent<Image>();
+    }
+}
---
+++ Assets/Scripts/SmellBarManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmellBarManager : MonoBehaviour
+{
+    [SerializeField] private BarManager[] _barManagers = new BarManager[0];
+    [SerializeField] private int _minSize;
+    [SerializeField] private int _maxSize;
+    [SerializeField, Range(0,1)] private float[] _barManagerSize = new float[0];
+
+    private void OnValidate()
+    {
+        if (_barManagers.Length != transform.childCount) {
+            _barManagers = new BarManager[transform.childCount];
+
+            for (int i = 0; i < transform.childCount; i++)
+                _barManagers[i] = transform.GetChild(i).GetComponent<BarManager>();
+        }
+
+        if (_barManagerSize.Length != transform.childCount)
+            _barManagerSize = new float[transform.childCount];
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            _barManagers[i].Size = _barManagerSize[i];
+            _barManagers[i].MinValue = _minSize;
+            _barManagers[i].MaxValue = _maxSize;
+            _barManagers[i].UpdateBar();
+        }
+    }
+
+}
